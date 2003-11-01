c71d791f3b20951cd8e5baa023de41fcb795dac8
tamlyn <tamlyn>
2003-11-01 14:27:23 +0000

    Wrote (pre) release notes.

--- templates/default/index.tpl.php
+++ templates/default/index.tpl.php
@@ -11,14 +11,18 @@
 //include header file
 include $sg->config->pathto_current_template."header.tpl.php";
 
-if($sg->isImage()) {
-  include $sg->config->pathto_current_template."image.tpl.php";
-} elseif($sg->isGallery()) {
-  include $sg->config->pathto_current_template."gallery.tpl.php";
-} else {
-  include $sg->config->pathto_current_template."listing.tpl.php";
+switch($sg->action) {
+case "addcomment" :
+  include $sg->config->pathto_current_template."addcomment.tpl.php";
+default :
+  if($sg->isImage()) {
+    include $sg->config->pathto_current_template."image.tpl.php";
+  } elseif($sg->isGallery()) {
+    include $sg->config->pathto_current_template."gallery.tpl.php";
+  } else {
+    include $sg->config->pathto_current_template."listing.tpl.php";
+  }
 }
-
 //include footer file
 include $sg->config->pathto_current_template."footer.tpl.php";
 
