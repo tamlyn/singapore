f517e5c7678df4c9d3eb6c31780a022a63a0d44d
tamlyn <tamlyn>
2004-02-02 16:28:45 +0000

    added pear::db stuff

---
+++ secret.ini.php
@@ -0,0 +1,20 @@
+; singapore secret configuration file <http://singapore.sourceforge.net>
+;
+; these options are hidden from web users
+;
+; <?php if(false): ?>
+
+[PEAR::DB]
+;
+; settings relevant to the PEAR::DB backend. 
+; You may ignore these if you are using the CSV or iifn backends.
+;
+
+
+db_user = tamlyn
+db_password = goosebutt
+
+
+;
+; <?php endif; ?>
+;
