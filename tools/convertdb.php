7a3e5191401e968b6f9414d61c87c07c512e530e
tamlyn <tamlyn>
2004-09-27 06:34:16 +0000

    updated upgrade script to include permissions

--- tools/convertdb.php
+++ tools/convertdb.php
@@ -1,5 +1,16 @@
 <?php 
 
+//permissions bit flags
+define("SG_GRP_READ",   1);
+define("SG_GRP_EDIT",   2);
+define("SG_GRP_ADD",    4);
+define("SG_GRP_DELETE", 8);
+define("SG_WLD_READ",   16);
+define("SG_WLD_EDIT",   32);
+define("SG_WLD_ADD",    64);
+define("SG_WLD_DELETE", 128);
+
+
 function getGallery($path) 
 {
   $gal = new sgGallery('.');
@@ -114,24 +125,48 @@ function putGallery($gallery, $path) {
   return $success;
 }
 
+function setPerms($obj) {
+  $obj->permissions = 0;
+  if(!empty($_POST["sgGrpRead"]))   $obj->permissions |= SG_GRP_READ;
+  if(!empty($_POST["sgGrpEdit"]))   $obj->permissions |= SG_GRP_EDIT;
+  if(!empty($_POST["sgGrpAdd"]))    $obj->permissions |= SG_GRP_ADD;
+  if(!empty($_POST["sgGrpDelete"])) $obj->permissions |= SG_GRP_DELETE;
+  if(!empty($_POST["sgWldRead"]))   $obj->permissions |= SG_WLD_READ;
+  if(!empty($_POST["sgWldEdit"]))   $obj->permissions |= SG_WLD_EDIT;
+  if(!empty($_POST["sgWldAdd"]))    $obj->permissions |= SG_WLD_ADD;
+  if(!empty($_POST["sgWldDelete"])) $obj->permissions |= SG_WLD_DELETE;
+  
+  $obj->groups = $_REQUEST["sgGroups"];
+  $obj->owner = $_REQUEST["sgOwner"];
+  
+  return $obj;
+}
+
 
 function convertDirectory ($path)
 {
   if (is_dir($path)) {
     $gallery = getGallery($path);
-    echo "Checking $path<br />\n";
+    echo "<ul><li>Checking $path<br />\n";
     if($gallery) {
-      if($gallery->summary != "")
+      if($gallery->summary != "" && !empty($_REQUEST["convertOverwrite"]))
         echo "Did NOT overwrite non-empty summary in $path<br />\n";
       else {
-        $gallery->summary = $gallery->desc;
+        if($_REQUEST["convertType"]!='none')
+          $gallery->summary = $gallery->desc;
         if($_REQUEST["convertType"]=='move')
           $gallery->desc = "";
-        if(putGallery($gallery,$path))
-          echo "Successfully converted $path<br />\n";
-        else
-          echo "Problem saving data file for $path<br />\n";
       }
+
+      $gallery = setPerms($gallery);
+      
+      for($i=0; $i<count($gallery->images); $i++)
+        $gallery->images[$i] = setPerms($gallery->images[$i]);
+      
+      if(putGallery($gallery,$path))
+        echo "Successfully converted $path<br />\n";
+      else
+        echo "Problem saving data file for $path<br />\n";
     } else
       echo "Skipping $path<br />\n";
     $d = dir($path);
@@ -142,6 +177,7 @@ function convertDirectory ($path)
         convertDirectory($path);
       }
     }
+    echo "</li></ul>\n";
   }
 }
 
@@ -162,31 +198,78 @@ function convertDirectory ($path)
 <?php 
 if(isset($_REQUEST["convertType"])) { 
 
-  include "../includes/config.class.php";
-  include "../includes/gallery.class.php";
-  include "../includes/image.class.php";
-  $config = new sgConfig("../singapore.ini");
+  include "includes/config.class.php";
+  include "includes/gallery.class.php";
+  include "includes/image.class.php";
+  $config = new sgConfig("singapore.ini");
   
-  $config->base_path = "../";
-    
-  convertDirectory(realpath($config->base_path.$config->pathto_galleries));
+  $config->base_path = "";
+  
+  //echo "<ul>\n";
+  convertDirectory($config->base_path.$config->pathto_galleries);
+  //echo "</ul>\n";
   
   echo "<p>All operations complete.</p>\n";
 
 } else { ?>
-<p>This will convert all your metadata files to make use of v0.9.10's new 
-gallery summary field. You can choose to either copy or move the old description 
-field:</p>
+<p>This will convert all your metadata files for singapore 0.9.10.</p>
 
-<form method="get" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-<p><input type="radio" class="radio" name="convertType" value="copy" /> Copy<br />
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+<h3>summary field</h3>
+<p>There is a new gallery summary field that is displayed instead of the 
+description in the parent gallery. You can choose to either copy or move the 
+old description field to the summary field or leave both untouched:</p>
+
+<p><input type="radio" class="radio" name="convertType" value="copy" checked="true" /> Copy<br />
 <input type="radio" class="radio" name="convertType" value="move" /> Move<br />
-<input type="submit" class="button" value="Go" /></p>
-</form>
+<input type="radio" class="radio" name="convertType" value="none" /> Neither<br />
+
+<p>By default only empty summary fields will be written to. Check this option to
+allow the summary field to be overwritten <input type="checkbox" class="checkbox" name="convertOverwrite" /></p>
+
+<h3>permissions</h3>
+
+<p>This version introduces multiple authorised users and image &amp; gallery 
+permissions. Please choose the default permissions that you would like all 
+objects to be set to. The default permissions selected below are recommended as 
+they will make all images &amp; galleries readable by everyone but only 
+modifiable by administrators. See the readme for more information on the 
+permissions model used by singapore.</p>
+<table>
+<tr>
+  <td>Owner</td>
+  <td><input type="text" name="sgOwner" value="__nobody__" /></td>
+</tr>
+<tr>
+  <td>Groups</td>
+  <td><input type="text" name="sgGroups" value="" /></td>
+</tr>
+<tr>
+  <td>Group permissions</td>
+  <td><div class="inputbox">
+    <input type="checkbox" class="checkbox" name="sgGrpRead" checked="true"/> Read
+    <input type="checkbox" class="checkbox" name="sgGrpEdit" /> Edit
+    <input type="checkbox" class="checkbox" name="sgGrpAdd" /> Add
+    <input type="checkbox" class="checkbox" name="sgGrpDelete" /> Delete
+  </div></td>
+</tr>
+<tr>
+  <td>World permissions</td>
+  <td><div class="inputbox">
+    <input type="checkbox" class="checkbox" name="sgWldRead" checked="true"/> Read
+    <input type="checkbox" class="checkbox" name="sgWldEdit" /> Edit
+    <input type="checkbox" class="checkbox" name="sgWldAdd" /> Add
+    <input type="checkbox" class="checkbox" name="sgWldDelete" /> Delete
+  </div></td>
+</tr>
+</table>
+
 
 <p>Please note that while the script will create backups of your metadata files 
 it is highly recommended that you create your own backups for added security.</p>
 
+<input type="submit" class="button" value="Go" /></p>
+</form>
 <?php } ?>
 
 <p><a href="index.html">Return</a> to tools.</p>
