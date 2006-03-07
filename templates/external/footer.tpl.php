7ec615d6d6c96b1add84d126f75cd90b58336def
tamlyn <tamlyn>
2006-03-07 07:56:12 +0000

    added rss template; removed external template

--- templates/external/footer.tpl.php
+++
@@ -1,9 +0,0 @@
-
-<div id="sgFooter"><p>
-  <?php echo $sg->allRightsReserved(); ?>
-  <?php echo $sg->copyrightMessage(); ?>
-  <br />
-  <?php echo $sg->poweredByVersion(); ?> 
-  <?php echo $sg->scriptExecTimeText(); ?> |
-  <?php echo $sg->adminLink(); ?>
-</p></div>
